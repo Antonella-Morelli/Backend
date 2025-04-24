f83468da241f009af6a452d751341fca49fcddbf
Antonella-Morelli <antonellamorelli1998@libero.it>
2025-04-24 13:06:29 +0200

    first commit

---
+++ 15-04-25/server/authors.js
@@ -0,0 +1,100 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = 3001
+const dbName = 'StriveBlog'
+
+app.use(cors())
+app.use(express.json())
+
+// SCHEMA AUTORE
+const authorSchema = new mongoose.Schema({
+    nome: { type: String, required: true },
+    cognome: { type: String, required: true },
+    email: { type: String, required: true },
+    dataDiNascita: { type: String, required: true },
+    avatar: { type: String, required: true }
+})
+
+const authorsModel = mongoose.model('Author', authorSchema)
+
+// BASE
+app.get('/', (req, res) => {
+    res.json({ message: 'App connect' })
+})
+
+// GET /authors - Lista autori
+app.get('/authors', async (req, res) => {
+
+    const page = Number(req.query.page)
+    const limit = Number(req.query.limit)
+
+    try {
+
+        const skip = (page - 1) * limit
+        const authors = await authorsModel.find().skip(skip).limit(limit)
+
+        res.status(200).json(authors)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+// GET /authors/:_id - Singolo autore
+app.get('/authors/:_id', async (req, res) => {
+    try {
+        const author = await authorsModel.findById(req.params._id)
+        res.status(200).json(author)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+// POST /authors - Crea nuovo autore
+app.post('/authors', async (req, res) => {
+    try {
+        const obj = req.body
+        const newAuthor = new authorsModel(obj)
+        const dbAuthor = await newAuthor.save()
+        res.status(200).json(dbAuthor)
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+})
+
+// PUT /authors/:_id - Modifica autore
+app.put('/authors/:_id', async (req, res) => {
+    try {
+        const updatedAuthor = await authorsModel.findByIdAndUpdate(req.params._id, req.body, { new: true })
+        res.status(200).json(updatedAuthor)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+// DELETE /authors/:_id - Elimina autore
+app.delete('/authors/:_id', async (req, res) => {
+    try {
+        await authorsModel.findByIdAndDelete(req.params._id)
+        res.status(200).json({ message: 'Autore eliminato con successo' })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+// Connessione al DB e avvio del server
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://antonellamorelli1998:Antonella.98@clusterantonella.fijtagm.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAntonella' + dbName)
+        app.listen(port, () => {
+            console.log(`Attivo su port ${port}`)
+        })
+    } catch (err) {
+        console.error('Errore nella connessione al DB:', err)
+    }
+}
+
+start()
+
---
+++ 15-04-25/server/posts.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+const port = 3002
+const dbName = 'StriveBlog'
+
+app.use(cors())
+app.use(express.json())
+
+// SCHEMA POST
+const postSchema = new mongoose.Schema({
+    category: { type: String, required: true },
+    title: { type: String, required: true },
+    cover: { type: String, required: true },
+    readTime: {
+        value: { type: Number, required: true },
+        unit: { type: String, required: true }
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author', required: true },
+    content: { type: String, required: true }
+})
+
+const postsModel = mongoose.model('Post', postSchema)
+
+// BASE
+app.get('/', (req, res) => {
+    res.json({ message: 'post service attivo' })
+})
+
+// GET /posts - Lista post
+app.get('/posts', async (req, res) => {
+
+    const page = Number(req.query.page)
+    const limit = Number(req.query.limit)
+
+    try {
+        const skip = (page - 1) * limit
+        const posts = await postsModel.find().populate('author').skip(skip).limit(limit)
+
+        const formattedPosts = posts.map(post => ({
+            _id: post._id,
+            title: post.title,
+            cover: post.cover,
+            author: {
+                name: `${post.author.nome} ${post.author.cognome}`,
+                avatar: post.author.avatar
+            }
+        }))
+
+        res.status(200).json(formattedPosts)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+// GET /posts/:_id - Singolo post
+app.get('/posts/:_id', async (req, res) => {
+    try {
+        const post = await postsModel.findById(req.params._id).populate('author')
+
+        const formattedPost = {
+            _id: post._id,
+            title: post.title,
+            cover: post.cover,
+            content: post.content,
+            author: {
+                name: `${post.author.nome} ${post.author.cognome}`,
+                avatar: post.author.avatar
+            }
+        }
+
+        res.status(200).json(formattedPost)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+// POST /posts - Crea nuovo post
+app.post('/posts', async (req, res) => {
+    try {
+        const newPost = new postsModel(req.body)
+        const savedPost = await newPost.save()
+        res.status(201).json(savedPost)
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+})
+
+// PUT /posts/:_id - Modifica post
+app.put('/posts/:_id', async (req, res) => {
+    try {
+        const updatedPost = await postsModel.findByIdAndUpdate(req.params._id, req.body, { new: true })
+        res.status(200).json(updatedPost)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+// DELETE /posts/:_id - Elimina post
+app.delete('/posts/:_id', async (req, res) => {
+    try {
+        await postsModel.findByIdAndDelete(req.params._id)
+        res.status(200).json({ message: 'Post eliminato' })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+// DB connection e start server
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://antonellamorelli1998:Antonella.98@clusterantonella.fijtagm.mongodb.net/' + dbName)
+        app.listen(port, () => {
+            console.log(`Attivo su port ${port}`)
+        })
+    } catch (err) {
+        console.error('Errore nella connessione al DB:', err)
+    }
+}
+
+start()
